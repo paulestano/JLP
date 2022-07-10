c1c9dba9d65fab3365405da475d4bb29565ba6c6
Paul Estano <paul.estano@inria.fr>
2022-07-10 12:14:28 +0200

    test jit compilation

--- tests/test_quant.py
+++ tests/test_quant.py
@@ -1,13 +1,22 @@
 from jlp import float_quantize_stochastic_cuda, float_quantize_nearest_cuda
 import jax.numpy as jnp
+import jax
 from jax import grad, jit, vmap
 from jax import random
 from jax.config import config
 
-config.update("jax_enable_x64", True)
+def build_compile_quantize(func, man_bits, exp_bits, subnormals, saturate):
+    compiled=lambda x:func(x,man_bits, exp_bits, subnormals, saturate)
+    return jax.jit(compiled)
 
 key = random.PRNGKey(0)
 size=3
 x = random.normal(key, (size, size), dtype=jnp.float32)
-qx = float_quantize_nearest_cuda(x, 8,7,True, True)
-qx2 = float_quantize_stochastic_cuda(x, 8,7,True, True)
+print(x)
+quantize_nearest = build_compile_quantize(float_quantize_nearest_cuda, 2,3,True,True)
+quantize_stochastic = build_compile_quantize(float_quantize_stochastic_cuda, 2,3,True,True)
+qx = quantize_nearest(x)
+qx2 = quantize_stochastic(x)
+print(qx)
+print(qx2)
+
